9cb39ecdab91e1e1929b8d1e0f14626d305903e7
MRCG-djeffries <djeffries@mrc.gm>
2023-04-12 16:07:00 +0000

    Delete index.js

--- python_shinylive/site/index.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('sw.js')
-      //createDB(tf)
-      .then(() => {
-        console.log('Service Worker Registered sw.js');
-        //createDB(tf);
-      });
-  }
