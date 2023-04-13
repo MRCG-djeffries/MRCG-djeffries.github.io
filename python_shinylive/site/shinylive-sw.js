fdbf810f65b6f91de56aa0b7704b742da163dd98
MRCG-djeffries <djeffries@mrc.gm>
2023-04-13 11:13:33 +0000

    Update shinylive-sw.js

--- python_shinylive/site/shinylive-sw.js
+++ python_shinylive/site/shinylive-sw.js
@@ -131,11 +131,10 @@ var version = "v6";
 self.addEventListener("install", (event) => {
   event.waitUntil(
     //Promise.all([self.skipWaiting(), caches.open(version + cacheName)]),
-     caches.open(cachename)
-      .then(function (cache) {
-        console.log(cachename + 'cache opened')
-        return cache.addAll(urlstocache)
-      })
+    Promise.all([self.skipWaiting(), caches.open(version + cacheName).then(function (cache) { 
+      console.log('cache opened')
+      return cache.addAll(urlstocache)
+    })]),
   );
 });
 self.addEventListener("activate", function(event) {
